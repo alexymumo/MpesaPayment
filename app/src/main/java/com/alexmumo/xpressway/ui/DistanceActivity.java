bb4bb9cedbf0d56719fd695faec31ca0dfe8e1c4
alexymumo <alexymumo2000@gmail.com>
2022-10-24 11:49:31 +0300

    update

--- app/src/main/java/com/alexmumo/xpressway/ui/DistanceActivity.java
+++ app/src/main/java/com/alexmumo/xpressway/ui/DistanceActivity.java
@@ -1,47 +1,21 @@
 package com.alexmumo.xpressway.ui;
 
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.alexmumo.xpressway.R;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.graphics.Color;
-import android.location.Location;
-import android.net.Uri;
-import android.graphics.Color;
-import android.os.AsyncTask;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -51,66 +25,85 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.maps.android.SphericalUtil;
+import com.google.firebase.database.DatabaseReference;
 
-import com.alexmumo.xpressway.R;
+public class DistanceActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class DistanceActivity extends AppCompatActivity implements OnMapReadyCallback{
-
-    private GoogleMap googleMap;
-    Location location;
-    LocationRequest locationRequest;
-    Marker startmarker, destinationmarker;
+    private GoogleMap mMap;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient googleApiClient;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    Marker entryMarker, exitMarker;
+    private DatabaseReference databaseReference;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        googleMap = googleMap;
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED
-            ) {
-            } else {
-                checkLocationPermission();
-            }
-        }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        double entrylatitude = -1.3340096;
+        double entrylongitude = 36.8379499;
+        double exitlatitude  = -1.2998277;
+        double exitlongitude = 36.7607878;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(entrylatitude, entrylongitude);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Entry"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(entrylatitude, exitlongitude), 15.0f));
+        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(entrylatitude, entrylongitude), 15.0f);
+
+
+        //Calculate distance
+        float[] results = new float[1];
+        Location.distanceBetween(entrylatitude, entrylongitude, exitlatitude, exitlongitude, results);
+        float distance = results[0];
+
+        int kilometres = (int) (distance/1000);
+        Toast.makeText(this, String.valueOf(kilometres)+ "Kilometres", Toast.LENGTH_LONG).show();
+
     }
 
-    private void checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                new AlertDialog.Builder(this)
-                        .setTitle("Location")
-                        .setMessage("Give permission")
-                        .setPositiveButton("OK", (dialogInterface, i) -> ActivityCompat.requestPermissions(DistanceActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1))
-                        .create()
-                        .show();
-            } else {
-                ActivityCompat.requestPermissions(DistanceActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-            }
-        }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
     }
 }
